0eac89ff8be119ab8b3f3006d6d311180acde4af
nzbozny <nezbozny@hotmail.com>
2024-03-18 19:22:25 -0400

    tighten up borders with a todo idea for tomorrow

--- src/ui.rs
+++ src/ui.rs
@@ -83,6 +83,14 @@ fn draw_spreadsheet(frame: &mut Frame, editor: &Editor) {
             let should_highlight = should_highlight_cell(editor, &text, col, row);
             let current_cell = (row == editor.cursor_position.row) && (col == editor.cursor_position.col);
 
+            // TODO: the row and the column with the selected cell should have all 4 borders
+            // can do this by comparing the current row/col with the selected cell's row/col,
+            // if curr_row < selected_row then only do top border, if curr_row > selected_row only
+            // do bottom border, if curr_row == selected_row do both. 
+            // Similarly, if curr_col < selected_col then only do left border, 
+            // if curr_col > selected_col then only do right border, and if curr_col ==
+            // selected_col do both borders. this will let you still make the currently selected
+            // cell have thickness
             let border_type = if should_highlight || current_cell {
                 ratatui::widgets::BorderType::Thick
             } else {
@@ -102,24 +110,50 @@ fn draw_spreadsheet(frame: &mut Frame, editor: &Editor) {
                 Borders::RIGHT
             };
 
-            if viewbox_row == 0 {
-                borders |= Borders::TOP | Borders::BOTTOM
+            if viewbox_row != constants::SHEET_VIEWBOX_HEIGHT - 1 {
+                borders |= Borders::TOP
             } else {
-                borders |= Borders::BOTTOM
+                borders |= Borders::TOP | Borders::BOTTOM
             }
+
+            // only affects first column
+            let top_left = if viewbox_col == 0 {
+                if viewbox_row == 0 {
+                    ratatui::symbols::line::NORMAL.horizontal_down
+                } else {
+                    ratatui::symbols::line::NORMAL.vertical_right
+                }
+            } else {
+                ratatui::symbols::line::NORMAL.horizontal
+            };
             
-            let border_set = if viewbox_col == 0 {
+            let top_right = if viewbox_col != constants::SHEET_VIEWBOX_WIDTH {
+                if viewbox_row != 0 {
+                    ratatui::symbols::line::CROSS
+                } else {
+                    ratatui::symbols::line::NORMAL.horizontal_down
+                }
+            } else {
+//                if viewbox_row != 0 {
+                    ratatui::symbols::line::THICK_CROSS
+//                } else {
+//                    ratatui::symbols::line::NORMAL.horizontal
+//                }
+            };
+
+            let border_set = if viewbox_col == constants::SHEET_VIEWBOX_WIDTH - 1 {
                 ratatui::symbols::border::PLAIN
             } else {
                 // TODO: https://ratatui.rs/how-to/layout/collapse-borders/ 
                 ratatui::symbols::border::Set {
-                    top_right: ratatui::symbols::line::NORMAL.horizontal,
+                    top_right, 
+                    top_left,
                     bottom_right: ratatui::symbols::line::NORMAL.horizontal_up,
                     ..ratatui::symbols::border::PLAIN
                 }
             };
 
-            let block = Block::new().border_set(border_set).borders(borders).border_type(border_type).border_style(border_style);
+            let block = Block::new().border_set(border_set).borders(borders).border_style(border_style);
 
             let widget = Paragraph::new(text).block(block);
 
@@ -178,7 +212,64 @@ fn should_highlight_cell(editor: &Editor, text: &str, col: usize, row: usize) ->
     false
 }
 
+fn draw_spreadsheet2(frame: &mut Frame, editor: &Editor) {
+    let mut size = frame.size();
+    size.width /= constants::SHEET_VIEWBOX_WIDTH;
+    size.height /= constants::SHEET_VIEWBOX_HEIGHT;
+
+    let layout = Layout::default().direction(Direction::Horizontal).constraints([
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+    ]).split(frame.size());
+
+    let mut sub_layouts: Vec<Rc<[Rect]>> = vec![];
+
+    for i in 0..10 {
+        sub_layouts.push(Layout::default().direction(Direction::Vertical).constraints([
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+        ]).split(layout[i]))
+    }
+
+    let border_type = ratatui::widgets::BorderType::Plain;
+
+    let border_style = Style::new().fg(ratatui::style::Color::White);
+
+            let borders1 = Borders::LEFT | Borders::TOP | Borders::BOTTOM;
+            let border_set1 = ratatui::symbols::border::PLAIN;
+            let block1 = Block::new().border_set(border_set1).borders(borders1).border_type(border_type).border_style(border_style);
+            let widget1 = Paragraph::new("block1").block(block1);
+
+            let borders2 = Borders::LEFT | Borders::RIGHT | Borders::TOP | Borders::BOTTOM;
+            let border_set2 = ratatui::symbols::border::Set {
+                top_left: ratatui::symbols::line::NORMAL.horizontal_down,
+                ..ratatui::symbols::border::PLAIN
+            };
+            let block2 = Block::new().border_set(border_set2).borders(borders2).border_style(border_style);
+            let widget2 = Paragraph::new("block2").block(block2);
+
+            frame.render_widget(widget1, sub_layouts[0][0]);
+            frame.render_widget(widget2, sub_layouts[1][0]);
+}
+
 pub fn draw(frame: &mut Frame, editor: &Editor) {
     draw_spreadsheet(frame, editor);
+//    draw_spreadsheet2(frame, editor);
     draw_status_message(frame, editor);
 }
